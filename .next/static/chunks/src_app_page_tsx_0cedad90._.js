f1e8823150983cd9679c1bae4776d54faaf87328
jmes1221 <jilmoree@gmail.com>
2025-05-28 10:07:50 +0800

    Add .next/static/chunks/src_app_page_tsx_0cedad90._.js 1748398068564

---
+++ .next/static/chunks/src_app_page_tsx_0cedad90._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_components_Map_tsx_db1db590._.js",
+  "static/chunks/_1a2ccb7a._.js"
+],
+    source: "dynamic"
+});
